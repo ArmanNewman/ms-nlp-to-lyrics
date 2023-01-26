18aa763318118ea4762e116e1e50b78c73cf51dd
ArmanNewman <armandojimenezrobles@gmail.com>
2023-01-26 01:44:39 -0600

    Test credentials

--- execute_pipeline.sh
+++ execute_pipeline.sh
@@ -1,1 +1,1 @@
-python get_unique_tracks.py && python get_track_genius_metadata.py && get_genius_lyrics.py
+python3 get_unique_tracks.py && python get_track_genius_metadata.py && get_genius_lyrics.py
