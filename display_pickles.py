6f034485787f04bea1f8b6d4b2af04cdf37ff0e0
ArmanNewman <armandojimenezrobles@gmail.com>
2023-01-26 20:32:45 -0600

    Shows all files with pickle extension & some stats

--- display_pickles.py
+++ display_pickles.py
@@ -1,13 +1,18 @@
+import os
 import pandas as pd
 
-# lyrics = pd.read_pickle("lyrics.pickle")
+files = os.listdir()
+pickle_files = [f for f in files if f.endswith("pickle")]
+for pickle in pickle_files:
+    print(pickle)
+    data = pd.read_pickle(pickle)
 
-lyrics = pd.read_pickle("genius_metadata.pickle")
+    print(f"{pickle} Data:")
+    print(data.info())
+    print(data.shape)
+    print(data.head(15))
 
-print("Lyrics Data:")
-print(lyrics.info())
-print(lyrics.shape)
-print(lyrics.head(15))
+    print("-" * 75, "\n")
 
 # for lyric in lyrics.lyrics:
 #     if not isinstance(lyric, str):
