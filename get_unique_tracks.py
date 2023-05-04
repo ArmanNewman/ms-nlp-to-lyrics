8c5383ef2613132ceddc0e9f024c63cfdca2d854
ArmanNewman <armandojimenezrobles@gmail.com>
2023-05-03 23:08:49 -0600

    Change search_term pattern to be 'simplified_title by main_artist'

--- get_unique_tracks.py
+++ get_unique_tracks.py
@@ -16,9 +16,11 @@ os.chdir("..")
 select_columns = ["uri", "artist_names", "track_name"]
 deduped_tracks = charts.loc[:, select_columns].drop_duplicates(subset="uri")
 print(f"Total unique tracks: {deduped_tracks.shape[0]}")
-deduped_tracks["search_term"] = (
-    deduped_tracks.artist_names + " " + deduped_tracks.track_name
-)
+
+# Simplify the search term
+main_artist = deduped_tracks.artist_names.apply(lambda x: x.split(",")[0])
+simplified_title = deduped_tracks.track_name.apply(lambda x: x.split(" - ")[0])
+deduped_tracks["search_term"] = simplified_title + " by " + main_artist
 deduped_tracks["search_term"] = deduped_tracks.search_term.str.lower()
 
 deduped_tracks.to_pickle("deduped_tracks.pickle")
